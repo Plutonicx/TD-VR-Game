9c44c55f01862ac1c24e0a7b7b0f68f99e9d7c9f
Stevan <stojanovich.stevan@live.com>
2017-01-24 19:31:06 +1100

    added (animated) water droplet
    and it moves towards the bear

---
+++ Assets/projectile_motion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile_motion : MonoBehaviour {
+
+    Transform dropletTransform;
+    public Transform enemyTransform; //make this getable on fixed update.
+
+    public float projectileSpeed = 20;
+
+	// Use this for initialization
+	void Awake() {
+        dropletTransform = GetComponent<Transform>();
+	}
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 targetVector = (enemyTransform.position- transform.position);
+        targetVector.Normalize();
+
+        transform.position += targetVector * projectileSpeed * Time.deltaTime;
+    }
+}
