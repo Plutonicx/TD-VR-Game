510a9f6feeb064c79c58b7d17e61badc0a35e9be
Stevan <stojanovich.stevan@live.com>
2017-01-24 23:59:35 +1100

    making cute robot model

--- Assets/projectile_motion.cs
+++ Assets/projectile_motion.cs
@@ -4,22 +4,45 @@ using UnityEngine;
 
 public class projectile_motion : MonoBehaviour {
 
-    Transform dropletTransform;
     public Transform enemyTransform; //make this getable on fixed update.
+
+    ParticleSystem hitParticles;
+    MeshRenderer dropletMesh;
+    bool isActive;
 
     public float projectileSpeed = 20;
 
 	// Use this for initialization
 	void Awake() {
-        dropletTransform = GetComponent<Transform>();
+        hitParticles = GetComponentInChildren<ParticleSystem>();
+        dropletMesh = GetComponentInChildren<MeshRenderer>();
+        isActive = true;
 	}
 
     // Update is called once per frame
     private void Update()
     {
         Vector3 targetVector = (enemyTransform.position- transform.position);
+
+        isTargetHit(targetVector);
+
         targetVector.Normalize();
 
         transform.position += targetVector * projectileSpeed * Time.deltaTime;
+    }
+
+    private void isTargetHit(Vector3 targetVector)
+    {
+        if (targetVector.sqrMagnitude <= 0.5)
+        {
+            if (isActive)
+            {
+                hitParticles.Play();
+                dropletMesh.enabled = false;
+                isActive = false;
+                Destroy(gameObject, 2f);
+            }
+
+        }
     }
 }
