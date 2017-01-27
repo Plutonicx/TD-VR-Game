0c731ec8d18d5c3e089f16ebb17dc880e3308f20
Stevan <stojanovich.stevan@live.com>
2017-01-27 19:43:31 +1100

    added water bullets

---
+++ Assets/My_Assets/Scripts/AmmoManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AmmoManager : MonoBehaviour {
+
+
+    public float spawnTime = 3f;
+    public Transform spawnPoint;
+    public GameObject enemy;
+    public float radius = 10.5f;
+
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+	
+	
+	void Spawn () {
+
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        // find the closest enemy
+
+        foreach (GameObject enmy in enemies)
+        {
+            float tempDistance = (enmy.GetComponent<Transform>().position - transform.position).sqrMagnitude;
+            if (Math.Sqrt(tempDistance) < radius)
+            {
+                Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+            }
+
+        }
+
+
+
+    }
+}
--- Assets/projectile_motion.cs
+++ Assets/projectile_motion.cs
@@ -1,16 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class projectile_motion : MonoBehaviour {
 
-    public Transform enemyTransform; //make this getable on fixed update.
+    Transform enemyTransform; //make this getable on fixed update.
 
     ParticleSystem hitParticles;
     MeshRenderer dropletMesh;
     bool isActive;
 
     public float projectileSpeed = 20;
+    public float radius = 10;
 
 	// Use this for initialization
 	void Awake() {
@@ -20,15 +22,39 @@ public class projectile_motion : MonoBehaviour {
 	}
 
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
-        Vector3 targetVector = (enemyTransform.position- transform.position);
+        if (enemyTransform == null)
+        {
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
 
-        isTargetHit(targetVector);
+            // find the closest enemy
+            float closestDistance = 999;
+            float tempDistance;
 
-        targetVector.Normalize();
+            foreach (GameObject enemy in enemies)
+            {
+                tempDistance = (enemy.GetComponent<Transform>().position - transform.position).sqrMagnitude;
+                if (Math.Sqrt(tempDistance) < closestDistance && Math.Sqrt(tempDistance) < radius)
+                {
+                    closestDistance = (float) Math.Sqrt(tempDistance);
+                    enemyTransform = enemy.GetComponent<Transform>();
+                }
 
-        transform.position += targetVector * projectileSpeed * Time.deltaTime;
+            }
+        }
+
+        if (enemyTransform != null){
+            //enemyTransform = lockedEnemy.GetComponent<Transform>();
+
+            Vector3 targetVector = (enemyTransform.position - transform.position);
+
+            isTargetHit(targetVector);
+
+            targetVector.Normalize();
+
+            transform.position += targetVector * projectileSpeed * Time.deltaTime;
+        }
     }
 
     private void isTargetHit(Vector3 targetVector)
