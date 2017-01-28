a99740441235ef1ad44eade00a6510a7bc60e394
Stevan <stojanovich.stevan@live.com>
2017-01-28 16:00:20 +1100

    Added camera movement with wasd keys

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    // Use this for initialization
+    Vector3 targetCamPos;
+
+    public float smoothing = 5f;
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        targetCamPos.Set(-1*h, 0f, -1*v);
+
+        targetCamPos = targetCamPos.normalized;
+
+        transform.position = Vector3.Lerp(transform.position, transform.position + targetCamPos, smoothing * Time.deltaTime);
+    }
+}
