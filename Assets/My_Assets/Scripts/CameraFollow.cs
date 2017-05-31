bda01a34255ab3ebb2fcee90534d896d55df705d
Stevan <stojanovich.stevan@live.com>
2017-05-31 18:07:43 +1000

    configuring bounds.

--- Assets/My_Assets/Scripts/CameraFollow.cs
+++ Assets/My_Assets/Scripts/CameraFollow.cs
@@ -18,11 +18,20 @@ public class CameraFollow : MonoBehaviour {
 	void Update () {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-        Debug.Log("camera position, x: " + targetCamPos.x.ToString() + " y: " + targetCamPos.y.ToString() + " z: " + targetCamPos.z.ToString());
+        Debug.Log("camera position, x: " + transform.position.x.ToString() + " y: " + transform.position.y.ToString() + " z: " + transform.position.z.ToString());
 
         targetCamPos.Set(-1*h, 0f, -1*v);
 
         targetCamPos = targetCamPos.normalized;
+
+        if (transform.position.x <= 5 || transform.position.x >= 50 || transform.position.z <= 5 || transform.position.z >= 50)
+        {
+            // do something.
+        }
+        else
+        {
+
+        }
 
         transform.position = Vector3.Lerp(transform.position, transform.position + targetCamPos, smoothing * Time.deltaTime);
     }
